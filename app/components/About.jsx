8ce09df62610f39a62cf2328d94d31e0e5b738ce
Lorenzo <Lorenzo@MacBook-Pro-di-Lorenzo.local>
2016-06-24 14:56:25 +0200

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,9 +13,21 @@ var React = require('react');
 
 var About = () => {
   return(
-    <div>
-      <h3>About Component</h3>
+
+    <div >
+      <h3 className="text-center">About</h3>
+      <p>Questa è una applicazione per prelevare il meteo da openweather, costruita con React.</p>
+      <p>
+        Qui alcuni degli strumenti che ho utilizzato:
+      </p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a></li> - Questo è il framework javascript
+          utilizzato.
+        <li><a href="http://openweathermap.org">Open Weather Map</a></li> - Ho usato le API di
+          Open Weather Map per ricerca il meteo di una città
+      </ul>
     </div>
+
 
   );
 };
